cc2417fe0149473165af4f458affe2d3bbe7788f
projectmiluju <dnjsdyd0712@naver.com>
2025-08-17 01:37:52 +0900

    feat: Gradle + Spring Boot 기반 프로젝트 구조 및 기본 설정 초기화 DP-229

---
+++ src/main/java/goorm/ddok/DdokApplication.java
@@ -0,0 +1,13 @@
+package goorm.ddok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goorm/ddok/DdokApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.ddok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
