2a003c0c0dc23cc93de771e8b3eeb697eaafba68
projectmiluju <dnjsdyd0712@naver.com>
2025-08-18 08:13:10 +0900

    feat: UserRepository에 사용자명과 전화번호로 사용자 조회 메서드 추가 DP-235

--- src/main/java/goorm/ddok/member/repository/UserRepository.java
+++ src/main/java/goorm/ddok/member/repository/UserRepository.java
@@ -3,6 +3,10 @@ package goorm.ddok.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import goorm.ddok.member.domain.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // 이름 + 휴대폰번호 회원 조회
+    Optional<User> findByUsernameAndPhoneNumber(String username, String phoneNumber);
 }
