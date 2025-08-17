e7c85faef9981cb280bba741224959964d11c8ec
projectmiluju <dnjsdyd0712@naver.com>
2025-08-18 08:31:47 +0900

    fix: SignUpResponse에서 닉네임 및 프로필 이미지 URL 필드 제거 DP-235

--- src/main/java/goorm/ddok/member/dto/response/SignUpResponse.java
+++ src/main/java/goorm/ddok/member/dto/response/SignUpResponse.java
@@ -15,9 +15,4 @@ public class SignUpResponse {
     @Schema(description = "사용자 실명", example = "홍길동")
     private String username;
 
-    @Schema(description = "닉네임", example = "고통스러운 개발자")
-    private String nickname;
-
-    @Schema(description = "프로필 이미지 URL", example = "https://cdn.pixabay.com/photo/2013/07/12/14/15/boy-148071_1280.png")
-    private String profileImageUrl;
 }
