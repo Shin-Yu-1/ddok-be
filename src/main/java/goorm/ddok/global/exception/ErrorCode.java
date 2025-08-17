1bf8ea58251df12a7b2ce058705b3894b99ec83d
projectmiluju <dnjsdyd0712@naver.com>
2025-08-18 06:03:40 +0900

    feat: 전역 에러 처리 설정 추가 DP-233

---
+++ src/main/java/goorm/ddok/global/exception/ErrorCode.java
@@ -0,0 +1,39 @@
+package goorm.ddok.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    // 400 BAD REQUEST
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 입력값입니다."),
+
+
+    // 401 UNAUTHORIZED
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
+
+
+
+    // 403 FORBIDDEN
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+
+
+    // 404 NOT FOUND
+    NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다."),
+
+
+
+    // 409 CONFLICT
+
+    // 500 INTERNAL SERVER ERROR
+    SMS_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "SMS 발송 실패");
+
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
