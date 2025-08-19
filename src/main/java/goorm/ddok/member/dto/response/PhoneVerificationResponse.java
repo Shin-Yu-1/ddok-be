8e444c1bd4afcd72be845a37f8d0e9db3917489c
projectmiluju <dnjsdyd0712@naver.com>
2025-08-20 00:45:33 +0900

    refactor: PhoneVerificationRequest, PhoneVerificationResponse, PhoneVerifyCodeRequest, and PhoneVerifyCodeResponse 클래스에 Swagger 스키마 추가 및 필드 설명 개선 DP-237

--- src/main/java/goorm/ddok/member/dto/response/PhoneVerificationResponse.java
+++ src/main/java/goorm/ddok/member/dto/response/PhoneVerificationResponse.java
@@ -5,11 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@Schema
 @AllArgsConstructor
+@Schema(
+        name = "PhoneVerificationResponse",
+        description = "휴대폰 인증 발송 응답 DTO",
+        example = """
+    { "expiresIn": 180 }
+    """
+)
 public class PhoneVerificationResponse {
 
-    @Schema(description = "인증 유효 시간", example =  "180")
+    @Schema(
+            description = "인증 유효 시간(초)",
+            example = "180",
+            type = "integer",
+            format = "int32",
+            accessMode = Schema.AccessMode.READ_ONLY
+    )
     private int expiresIn;
 }
-
--- src/main/java/goorm/ddok/member/dto/response/PhoneVerifyCodeResponse.java
+++ src/main/java/goorm/ddok/member/dto/response/PhoneVerifyCodeResponse.java
@@ -6,9 +6,19 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-@Schema(description = "휴대폰 인증 코드 응답 DTO")
+@Schema(
+        name = "PhoneVerifyCodeResponse",
+        description = "휴대폰 인증 코드 응답 DTO",
+        example = """
+    { "verified": true }
+    """
+)
 public class PhoneVerifyCodeResponse {
 
-    @Schema(description = "인증 확인 여부", example =  "true")
+    @Schema(
+            description = "인증 확인 여부",
+            example = "true",
+            accessMode = Schema.AccessMode.READ_ONLY
+    )
     private boolean verified;
 }
