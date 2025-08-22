1f73395a00ffd5969e2d107961346b328d6cc51e
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-22 10:21:07 +0900

    fix(domain): ChatRoom 수정 DP-260

--- src/main/java/goorm/ddok/chat/domain/ChatRoom.java
+++ src/main/java/goorm/ddok/chat/domain/ChatRoom.java
@@ -1,8 +1,22 @@
 package goorm.ddok.chat.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import goorm.ddok.member.domain.User;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.Check;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.Instant;
+
+@Check(constraints = """
+    (room_type = 'PRIVATE' AND private_a_user_id IS NOT NULL AND private_b_user_id IS NOT NULL
+     AND private_a_user_id <> private_b_user_id AND owner_user_id IS NULL)
+    OR
+    (room_type = 'GROUP' AND owner_user_id IS NOT NULL
+     AND private_a_user_id IS NULL AND private_b_user_id IS NULL)
+""")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,11 +31,40 @@ public class ChatRoom {
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "room_type", nullable = false)
+    @Column(nullable = false)
     private  ChatRoomType roomType;
 
-    @Column(name = "owner_user_id")
-    private Long ownerUserId;
+    @Column()
+    private String name;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_chat_room_owner_user"))
+    @ToString.Exclude
+    @JsonIgnore
+    private User ownerUserId;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_chat_room_private_a_user"))
+    @ToString.Exclude
+    @JsonIgnore
+    private User privateAUserId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_chat_room_private_b_user"))
+    @ToString.Exclude
+    @JsonIgnore
+    private User privateBUserId;
+
+    @Column()
+    private Instant lastMessageAt;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    @Setter(AccessLevel.NONE)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    @Setter(AccessLevel.NONE)
+    private Instant updatedAt;
 }
