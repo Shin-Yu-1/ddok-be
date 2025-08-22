7ca7861156082fa655e90d1ebc249e9b43c44b1a
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-22 11:00:03 +0900

    feat(domain): ChatMessage 추가 DP-260

---
+++ src/main/java/goorm/ddok/chat/domain/ChatMessage.java
@@ -0,0 +1,72 @@
+package goorm.ddok.chat.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import goorm.ddok.member.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "chat_message",
+        indexes = {@Index(name = "idx_chat_message_room_created", columnList = "room_id, created_at")})
+@EntityListeners(AuditingEntityListener.class)
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long roomId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(insertable = false, updatable = false)
+    @ToString.Exclude
+    @JsonIgnore
+    private ChatRoom chatRoom;
+
+    @Column()
+    private Long senderId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(insertable = false, updatable = false)
+    @ToString.Exclude
+    @JsonIgnore
+    private User sender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private ChatContentType contentType = ChatContentType.TEXT;
+
+    @Column(columnDefinition = "TEXT")
+    private String contentText;
+
+    @Column(columnDefinition = "TEXT")
+    private String fileUrl;
+
+    @Column()
+    private Long replyToId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(insertable = false, updatable = false)
+    @ToString.Exclude
+    @JsonIgnore
+    private ChatMessage replyTo;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    @Setter(AccessLevel.NONE)
+    private Instant createdAt;
+
+    @Column()
+    private Instant deletedAt;
+}
