b2db84121254c221612a4a3852eb1f36c5c127d3
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-23 17:58:36 +0900

    feat(dto): response에 필요한 dto 추가 DP-260

---
+++ src/main/java/goorm/ddok/chat/dto/response/PaginationDto.java
@@ -0,0 +1,27 @@
+package goorm.ddok.chat.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "페이징 정보")
+public class PaginationDto {
+
+    @Schema(description = "현재 페이지")
+    private Integer currentPage;
+
+    @Schema(description = "페이지 크기")
+    private Integer pageSize;
+
+    @Schema(description = "전체 페이지 수")
+    private Integer totalPages;
+
+    @Schema(description = "전체 항목 수")
+    private Long totalItems;
+}
