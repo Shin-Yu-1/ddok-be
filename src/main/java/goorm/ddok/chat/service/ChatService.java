b1a5766d1891f2181d1356388a6745a35fd8bb15
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-23 21:31:23 +0900

    fix(service): 매개변수에 userID에서 email로 변경 및 userID 조회 추가 DP-260

--- src/main/java/goorm/ddok/chat/service/ChatService.java
+++ src/main/java/goorm/ddok/chat/service/ChatService.java
@@ -4,6 +4,9 @@ import goorm.ddok.chat.domain.ChatRoom;
 import goorm.ddok.chat.dto.response.*;
 import goorm.ddok.chat.repository.ChatRepository;
 import goorm.ddok.chat.util.ChatMapper;
+import goorm.ddok.global.exception.ErrorCode;
+import goorm.ddok.global.exception.GlobalException;
+import goorm.ddok.member.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -21,11 +24,18 @@ import java.util.stream.Collectors;
 public class ChatService {
 
     private final ChatRepository chatRepository;
+    private final UserRepository userRepository;
     private final ChatMapper chatMapper;
 
 
     // 1:1 채팅 목록 조회
-    public ChatListResponseDto getPrivateChats(Long userId, Pageable pageable) {
+    public ChatListResponseDto getPrivateChats(String email, Pageable pageable) {
+
+        Long userId = userRepository.findByEmail(email).orElseThrow(RuntimeException::new).getId();
+
+        if (userId == null) {
+            throw new GlobalException(ErrorCode.USER_NOT_FOUND);
+        }
 
         Page<ChatRoom> chatRoomPage = chatRepository.findPrivateChatsByUserId(userId, pageable);
 
@@ -47,7 +57,13 @@ public class ChatService {
     }
 
     // 팀 채팅 목록 조회
-    public ChatListResponseDto getTeamChats(Long userId, Pageable pageable) {
+    public ChatListResponseDto getTeamChats(String email, Pageable pageable) {
+
+        Long userId = userRepository.findByEmail(email).orElseThrow(RuntimeException::new).getId();
+
+        if (userId == null) {
+            throw new GlobalException(ErrorCode.USER_NOT_FOUND);
+        }
 
         Page<ChatRoom> chatRoomPage = chatRepository.findTeamChatsByUserId(userId, pageable);
 
