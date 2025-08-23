dcdc5e52f91376e76b5a10d4433a94b619117243
sunsetkk <khyejin9796@gmail.com>
2025-08-24 03:45:34 +0900

    feat(project): 프로젝트 상세조회 컨트롤러 추가 DP-267

--- src/main/java/goorm/ddok/project/controller/ProjectRecruitmentReadController.java
+++ src/main/java/goorm/ddok/project/controller/ProjectRecruitmentReadController.java
@@ -1,2 +1,36 @@
-package goorm.ddok.project.controller;public class ProjectRecruitmentReadController {
+package goorm.ddok.project.controller;
+
+
+import goorm.ddok.global.response.ApiResponseDto;
+import goorm.ddok.global.security.auth.CustomUserDetails;
+import goorm.ddok.project.dto.response.ProjectDetailResponse;
+import goorm.ddok.project.service.ProjectRecruitmentReadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/projects")
+@RequiredArgsConstructor
+public class ProjectRecruitmentReadController {
+
+    private final ProjectRecruitmentReadService projectRecruitmentReadService;
+
+    /**
+     * 프로젝트 모집글 상세 조회
+     */
+    @GetMapping("/{projectId}")
+    public ResponseEntity<ApiResponseDto<ProjectDetailResponse>> getProjectDetail(
+            @PathVariable Long projectId,
+            @AuthenticationPrincipal CustomUserDetails user
+    ) {
+        ProjectDetailResponse response = projectRecruitmentReadService.getProjectDetail(projectId, user);
+        return ResponseEntity.ok(
+                ApiResponseDto.of(200, "프로젝트 모집글 상세 조회가 성공했습니다.", response)
+        );
+    }
 }
