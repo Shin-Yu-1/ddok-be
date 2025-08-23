af011afa3c050404ac2cd523d1c18a3e55e2c86b
sunsetkk <khyejin9796@gmail.com>
2025-08-24 03:46:02 +0900

    feat(project): ProjectApplicationRepository 추가 (지원자 수 카운트용) DP-267

--- src/main/java/goorm/ddok/project/repository/ProjectApplicationRepository.java
+++ src/main/java/goorm/ddok/project/repository/ProjectApplicationRepository.java
@@ -1,2 +1,9 @@
-package goorm.ddok.project.repository;public interface ProjectApplicationRepository {
+package goorm.ddok.project.repository;
+
+import goorm.ddok.project.domain.ProjectApplication;
+import goorm.ddok.project.domain.ProjectRecruitment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectApplicationRepository extends JpaRepository<ProjectApplication, Long> {
+    int countByProjectRecruitment(ProjectRecruitment recruitment);
 }
