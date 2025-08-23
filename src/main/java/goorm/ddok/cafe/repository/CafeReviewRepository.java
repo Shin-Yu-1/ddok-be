68144a71d5e7bb6444c8660da94f7401128326a6
vayaconchoi <chlqjarms77@gmail.com>
2025-08-23 19:19:53 +0900

    feat(쓰지 않는 import 및 param 수정): 쓰지 않는 import 및 param 수정 DP-264

--- src/main/java/goorm/ddok/cafe/repository/CafeReviewRepository.java
+++ src/main/java/goorm/ddok/cafe/repository/CafeReviewRepository.java
@@ -3,7 +3,7 @@ package goorm.ddok.cafe.repository;
 import goorm.ddok.cafe.domain.Cafe;
 import goorm.ddok.cafe.domain.CafeReview;
 import goorm.ddok.member.domain.User;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
