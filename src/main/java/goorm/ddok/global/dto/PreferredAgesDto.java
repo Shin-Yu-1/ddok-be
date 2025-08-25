c9a4f0c0e664602eb5f4db6f4fde91c62da00659
sunsetkk <khyejin9796@gmail.com>
2025-08-25 10:45:02 +0900

    refactor(global): 검증 어노테이션 추가 DP-266

--- src/main/java/goorm/ddok/global/dto/PreferredAgesDto.java
+++ src/main/java/goorm/ddok/global/dto/PreferredAgesDto.java
@@ -1,6 +1,7 @@
 package goorm.ddok.global.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -9,9 +10,11 @@ import lombok.*;
 @Builder
 @Schema(name = "PreferredAgesDto", description = "선호 연령대")
 public class PreferredAgesDto {
+    @NotNull(message = "최소 연령은 필수 입력값입니다.")
     @Schema(description = "최소 연령", example = "20")
     private Integer ageMin;
 
+    @NotNull(message = "최대 연령은 필수 입력값입니다.")
     @Schema(description = "최대 연령", example = "30")
     private Integer ageMax;
 }
