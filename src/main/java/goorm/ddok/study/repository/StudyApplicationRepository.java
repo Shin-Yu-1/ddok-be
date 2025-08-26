9cbc824be8e3f75ceaf49f717de5124d229f3a7d
projectmiluju <dnjsdyd0712@naver.com>
2025-08-27 00:54:29 +0900

    feat: find StudyApplication by user and study recruitment IDs 메서드 추가 DP-271

--- src/main/java/goorm/ddok/study/repository/StudyApplicationRepository.java
+++ src/main/java/goorm/ddok/study/repository/StudyApplicationRepository.java
@@ -3,5 +3,9 @@ package goorm.ddok.study.repository;
 import goorm.ddok.study.domain.StudyApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudyApplicationRepository extends JpaRepository<StudyApplication, Long> {
+
+    Optional<StudyApplication> findByUser_IdAndStudyRecruitment_Id(Long userId, Long studyId);
 }
