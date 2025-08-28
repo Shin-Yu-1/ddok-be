af50d199bd52377bc003e4fc5c45ad7d25332205
sunsetkk <khyejin9796@gmail.com>
2025-08-29 02:08:26 +0900

    feat(player): 프로필 기술 스택 조회 서비스 구현 DP-288

--- src/main/java/goorm/ddok/player/service/ProfileStackQueryService.java
+++ src/main/java/goorm/ddok/player/service/ProfileStackQueryService.java
@@ -1,5 +1,8 @@
-package goorm.ddok.player;
+package goorm.ddok.player.service;
 
+import goorm.ddok.global.exception.ErrorCode;
+import goorm.ddok.global.exception.GlobalException;
+import goorm.ddok.member.repository.UserRepository;
 import goorm.ddok.member.repository.UserTechStackRepository;
 import goorm.ddok.player.dto.response.TechStackResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +16,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ProfileStackQueryService {
 
+    private final UserRepository userRepository;
     private final UserTechStackRepository userTechStackRepository;
 
     /**
-     * 사용자 기술스택 상세 조회
+     * 사용자 기술 스택 조회
      */
     public Page<TechStackResponse> getUserTechStacks(Long userId, Pageable pageable) {
+        // 1. 유저 존재 여부 체크
+        if (!userRepository.existsById(userId)) {
+            throw new GlobalException(ErrorCode.USER_NOT_FOUND);
+        }
+
+        // 2. 기술 스택 조회
         return userTechStackRepository.findByUserId(userId, pageable)
                 .map(uts -> TechStackResponse.builder()
                         .techStack(uts.getTechStack().getName())
