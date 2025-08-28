d377ebd6be4f525c59a08bcd4846dff62898c253
sunsetkk <khyejin9796@gmail.com>
2025-08-28 23:46:06 +0900

    refactor(member): 온도 타입 double -> BigDecimal 변경 DP-283

--- src/main/java/goorm/ddok/member/repository/UserPortfolioRepository.java
+++ src/main/java/goorm/ddok/member/repository/UserPortfolioRepository.java
@@ -1,11 +1,14 @@
 package goorm.ddok.member.repository;
 
+import goorm.ddok.member.domain.User;
 import goorm.ddok.member.domain.UserPortfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface UserPortfolioRepository extends JpaRepository<UserPortfolio, Long> {
+
+    List<UserPortfolio> findByUser(User user);
 
     List<UserPortfolio> findAllByUserId(Long userId);
 }
