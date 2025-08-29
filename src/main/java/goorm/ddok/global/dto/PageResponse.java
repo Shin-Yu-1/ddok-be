5439e052199915a8c3b7bb17a52df516b24f7d89
sunsetkk <khyejin9796@gmail.com>
2025-08-29 12:11:35 +0900

    refactor(profile): PageResponse 구조 개선 DP-288

--- src/main/java/goorm/ddok/global/dto/PageResponse.java
+++ src/main/java/goorm/ddok/global/dto/PageResponse.java
@@ -10,33 +10,46 @@ import org.springframework.data.domain.Page;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Schema(description = "페이지네이션 응답 DTO")
 public class PageResponse<T> {
 
-    @Schema(description = "현재 페이지", example = "0")
-    private final int currentPage;
-
-    @Schema(description = "페이지 크기", example = "10")
-    private final int pageSize;
-
-    @Schema(description = "전체 페이지 수", example = "1")
-    private final int totalPages;
-
-    @Schema(description = "전체 아이템 수", example = "3")
-    private final long totalItems;
+    @Schema(description = "페이지네이션 정보")
+    private Pagination pagination;
 
     @Schema(description = "조회 데이터 목록")
-    private final List<T> items;
+    private List<T> items;
 
-    private PageResponse(Page<T> page) {
-        this.currentPage = page.getNumber();
-        this.pageSize = page.getSize();
-        this.totalPages = page.getTotalPages();
-        this.totalItems = page.getTotalElements();
-        this.items = page.getContent();
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Pagination {
+        @Schema(description = "현재 페이지", example = "0")
+        private int currentPage;
+
+        @Schema(description = "페이지 크기", example = "10")
+        private int pageSize;
+
+        @Schema(description = "전체 페이지 수", example = "1")
+        private int totalPages;
+
+        @Schema(description = "전체 아이템 수", example = "3")
+        private long totalItems;
     }
 
     public static <T> PageResponse<T> of(Page<T> page) {
-        return new PageResponse<>(page);
+        return PageResponse.<T>builder()
+                .pagination(Pagination.builder()
+                        .currentPage(page.getNumber())
+                        .pageSize(page.getSize()) // 요청한 사이즈 (예: 14)
+                        .totalPages(page.getTotalPages())
+                        .totalItems(page.getTotalElements())
+                        .build())
+                .items(page.getContent())
+                .build();
     }
 }
