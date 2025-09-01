2033f181b22eb22789d3e7185cdfef516951e383
Shin-Yu-1 <shingyu.user@gmail.com>
2025-09-01 10:41:22 +0900

    feat(service): 채팅 읽음 처리 응답 DTO 추가 DP-293

---
+++ src/main/java/goorm/ddok/chat/dto/response/ChatReadResponse.java
@@ -0,0 +1,20 @@
+package goorm.ddok.chat.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Schema(
+        name = "ChatReadResponse",
+        description = "채팅 메세지 읽음 처리 응답"
+)
+
+public class ChatReadResponse {
+
+    @Schema(description = "메세지 ID", example = "1")
+    private Long messageId;
+}
