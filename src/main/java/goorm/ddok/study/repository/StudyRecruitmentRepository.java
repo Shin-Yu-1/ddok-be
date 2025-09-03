c8e18910edc904a182c6d237ebbb8d5f4f87e9c6
projectmiluju <dnjsdyd0712@naver.com>
2025-09-04 00:44:57 +0900

    feat: 지도 범위 내 스터디 모집 정보 조회 기능 추가 DP-272

--- src/main/java/goorm/ddok/study/repository/StudyRecruitmentRepository.java
+++ src/main/java/goorm/ddok/study/repository/StudyRecruitmentRepository.java
@@ -1,12 +1,15 @@
 package goorm.ddok.study.repository;
 
+import goorm.ddok.project.domain.TeamStatus;
 import goorm.ddok.study.domain.StudyRecruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 public interface StudyRecruitmentRepository extends JpaRepository<StudyRecruitment, Long> {
@@ -16,4 +19,49 @@ public interface StudyRecruitmentRepository extends JpaRepository<StudyRecruitme
     @Query("update StudyRecruitment s set s.deletedAt = :now where s.id = :id and s.deletedAt is null")
     int softDeleteById(@Param("id") Long id, @Param("now") Instant now);
 
+
+    interface MapRow {
+        Long getId();
+        String getTitle();
+        TeamStatus getTeamStatus();
+
+        String getRegion1depthName();
+        String getRegion2depthName();
+        String getRegion3depthName();
+        String getRoadName();
+        String getMainBuildingNo();
+        String getSubBuildingNo();
+        String getZoneNo();
+
+        BigDecimal getLatitude();
+        BigDecimal getLongitude();
+    };
+
+    @Query("""
+        select
+          sr.id as id,
+          sr.title as title,
+          sr.teamStatus as teamStatus,
+          sr.region1depthName as region1depthName,
+          sr.region2depthName as region2depthName,
+          sr.region3depthName as region3depthName,
+          sr.roadName as roadName,
+          sr.mainBuildingNo as mainBuildingNo,
+          sr.subBuildingNo as subBuildingNo,
+          sr.zoneNo as zoneNo,
+          sr.latitude as latitude,
+          sr.longitude as longitude
+        from StudyRecruitment sr
+        where sr.deletedAt is null
+          and sr.latitude is not null
+          and sr.longitude is not null
+          and sr.latitude between :swLat and :neLat
+          and sr.longitude between :swLng and :neLng
+    """)
+    List<StudyRecruitmentRepository.MapRow> findAllInBounds(
+            @Param("swLat") BigDecimal swLat,
+            @Param("neLat") BigDecimal neLat,
+            @Param("swLng") BigDecimal swLng,
+            @Param("neLng") BigDecimal neLng
+    );
 }
