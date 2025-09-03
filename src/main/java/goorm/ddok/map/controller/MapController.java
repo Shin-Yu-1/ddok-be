b2da3feac466cd06bcafbda7f6b89acfabd14928
projectmiluju <dnjsdyd0712@naver.com>
2025-09-04 00:47:12 +0900

    feat: 지도 범위 내 스터디 조회 API 구현 DP-272

--- src/main/java/goorm/ddok/map/controller/MapController.java
+++ src/main/java/goorm/ddok/map/controller/MapController.java
@@ -1,6 +1,5 @@
 package goorm.ddok.map.controller;
 
-import goorm.ddok.cafe.dto.response.CafeMapItemResponse;
 import goorm.ddok.global.response.ApiResponseDto;
 import goorm.ddok.map.dto.response.ProjectMapItemResponse;
 import goorm.ddok.map.dto.response.StudyMapItemResponse;
@@ -38,7 +37,7 @@ public class MapController {
     @Operation(
             summary = "프로젝트 전체 조회(지도 범위)",
             description = """
-                지도 영역(bounding box) 내의 카페를 조회합니다.
+                지도 영역(bounding box) 내의 프로젝트를 조회합니다.
                 - deletedAt IS NULL 만 반환합니다.
                 - swLat ≤ neLat, swLng ≤ neLng 이어야 합니다.
                 """
@@ -130,6 +129,78 @@ public class MapController {
         return ResponseEntity.ok(ApiResponseDto.of(200, "지도 프로젝트 조회에 성공하였습니다.", data));
     }
 
+
+    @Operation(
+            summary = "스터디 전체 조회(지도 범위)",
+            description = """
+                지도 영역(bounding box) 내의 스터디를 조회합니다.
+                - deletedAt IS NULL 만 반환합니다.
+                - swLat ≤ neLat, swLng ≤ neLng 이어야 합니다.
+                """
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공",
+            content = @Content(schema = @Schema(implementation = ApiResponseDto.class),
+                    examples = @ExampleObject(name = "성공 예시", value = """
+                    {
+                      "status": 200,
+                      "message": "지도 스터디 조회에 성공하였습니다.",
+                      "data": [
+                        {
+                          "category": "study",
+                          "projectId": 1,
+                          "title": "구지라지 스터디",
+                          "teamStatus": "RECRUITING",
+                          "location": {
+                            "address": "부산 해운대구 우동 센텀중앙로 90",
+                            "region1depthName": "부산",
+                            "region2depthName": "해운대구",
+                            "region3depthName": "우동",
+                            "roadName": "센텀중앙로",
+                            "mainBuildingNo": "90",
+                            "subBuildingNo": "",
+                            "zoneNo": "48058",
+                            "latitude": 35.1702,
+                            "longitude": 129.1270
+                          }
+                        },
+                        {
+                          "category": "study",
+                          "projectId": 2,
+                          "title": "구라라지 스터디",
+                          "teamStatus": "ONGOING",
+                          "location": {
+                            "address": "부산 해운대구 우동 센텀중앙로 90",
+                            "region1depthName": "부산",
+                            "region2depthName": "해운대구",
+                            "region3depthName": "우동",
+                            "roadName": "센텀중앙로",
+                            "mainBuildingNo": "90",
+                            "subBuildingNo": "",
+                            "zoneNo": "48058",
+                            "latitude": 35.1702,
+                            "longitude": 129.1270
+                          }
+                        }
+                      ]
+                    }
+                    """))),
+            @ApiResponse(responseCode = "400", description = "잘못된 경계값",
+                    content = @Content(schema = @Schema(implementation = ApiResponseDto.class),
+                            examples = @ExampleObject(name = "경계 오류 예시", value = """
+                    { "status": 400, "message": "잘못된 지도 경계값입니다.", "data": null }
+                    """))),
+            @ApiResponse(responseCode = "400", description = "필수 파라미터 누락",
+                    content = @Content(schema = @Schema(implementation = ApiResponseDto.class),
+                            examples = @ExampleObject(name = "경계 오류 예시", value = """
+                    { "status": 400, "message": "필수 파라미터가 누락되었습니다.", "data": null }
+                    """))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류",
+                    content = @Content(schema = @Schema(implementation = ApiResponseDto.class),
+                            examples = @ExampleObject(value = """
+                    { "status": 500, "message": "서버 내부 오류", "data": null }
+                    """)))
+    })
     @GetMapping("/studies")
     public ResponseEntity<ApiResponseDto<List<StudyMapItemResponse>>> getStudies(
             @Parameter(description = "남서쪽 위도", example = "37.55")
