7a085b0904be62e9f03e044144d751d3def4c5e8
sunsetkk <khyejin9796@gmail.com>
2025-09-04 18:24:20 +0900

    feature(team): repository 추가 DP-324

--- src/main/java/goorm/ddok/team/repository/TeamRepository.java
+++ src/main/java/goorm/ddok/team/repository/TeamRepository.java
@@ -1,2 +1,7 @@
-package goorm.ddok.team.repository;public class TeamRepository {
+package goorm.ddok.team.repository;
+
+import goorm.ddok.team.domain.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
