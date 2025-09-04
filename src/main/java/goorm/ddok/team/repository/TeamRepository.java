0ac15fb4d4d573e806872b0ccabf34d58b19604d
sunsetkk <khyejin9796@gmail.com>
2025-09-04 20:07:04 +0900

    feature(team): recruitmentId+type 기준 팀 조회 메서드 추가 DP-317

--- src/main/java/goorm/ddok/team/repository/TeamRepository.java
+++ src/main/java/goorm/ddok/team/repository/TeamRepository.java
@@ -1,7 +1,13 @@
 package goorm.ddok.team.repository;
 
 import goorm.ddok.team.domain.Team;
+import goorm.ddok.team.domain.TeamType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Optional<Team> findByRecruitmentIdAndType(Long recruitmentId, TeamType type);
+
 }
