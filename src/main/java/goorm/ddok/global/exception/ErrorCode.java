3d000a8d6598121f305eddcff4e66eafb8645b77
projectmiluju <dnjsdyd0712@naver.com>
2025-09-05 09:03:53 +0900

    feat: 에러 코드 추가 - 리소스 미발견 및 지원하지 않는 카테고리 DP-321

--- src/main/java/goorm/ddok/global/exception/ErrorCode.java
+++ src/main/java/goorm/ddok/global/exception/ErrorCode.java
@@ -53,6 +53,8 @@ public enum ErrorCode {
     INVALID_FILE(HttpStatus.BAD_REQUEST, "부적절한 파일입니다."),
     FILE_TOO_LARGE(HttpStatus.BAD_REQUEST, "파일 크기는 5MB를 넘을 수 없습니다."),
     FILE_UPLOAD_FAILED(HttpStatus.BAD_REQUEST, "파일 업로드에 실패하였습니다."),
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없습니다."),
+    NOT_SUPPORT_CATEGORY(HttpStatus.BAD_REQUEST, "지원하지 않는 카테고리입니다."),
 
     // 401 UNAUTHORIZED
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
