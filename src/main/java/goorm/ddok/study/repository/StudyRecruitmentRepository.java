fda123e37ff3aabe19add748a88a25e9c1944701
projectmiluju <dnjsdyd0712@naver.com>
2025-09-05 13:18:39 +0900

    fix: StudyRecruitmentRepository에서 사용하지 않는 import 문 제거 DP-331

--- src/main/java/goorm/ddok/study/repository/StudyRecruitmentRepository.java
+++ src/main/java/goorm/ddok/study/repository/StudyRecruitmentRepository.java
@@ -4,7 +4,6 @@ import goorm.ddok.study.domain.TeamStatus;
 import goorm.ddok.study.domain.StudyRecruitment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
