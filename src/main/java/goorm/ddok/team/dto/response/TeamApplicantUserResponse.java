ca4fbb940abf828476f6679899b154f103899203
sunsetkk <khyejin9796@gmail.com>
2025-09-05 20:02:23 +0900

    feature(team): TeamApplicantUserResponse DTO 보완 (대표 뱃지/탈주 뱃지) DP-328

--- src/main/java/goorm/ddok/team/dto/response/TeamApplicantUserResponse.java
+++ src/main/java/goorm/ddok/team/dto/response/TeamApplicantUserResponse.java
@@ -1,5 +1,7 @@
 package goorm.ddok.team.dto.response;
 
+import goorm.ddok.badge.domain.BadgeType;
+import goorm.ddok.badge.domain.UserBadge;
 import goorm.ddok.global.dto.AbandonBadgeDto;
 import goorm.ddok.global.dto.BadgeDto;
 import goorm.ddok.member.domain.User;
@@ -8,6 +10,7 @@ import goorm.ddok.member.domain.UserPositionType;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
 
 @Getter
 @Builder
@@ -25,26 +28,4 @@ public class TeamApplicantUserResponse {
     private BadgeDto mainBadge;
     private AbandonBadgeDto abandonBadge;
 
-
-    public static TeamApplicantUserResponse from(User user) {
-        return TeamApplicantUserResponse.builder()
-                .userId(user.getId())
-                .nickname(user.getNickname())
-                .profileImageUrl(user.getProfileImageUrl())
-                .temperature(user.getReputation().getTemperature())
-                .mainPosition(resolvePrimaryPosition(user))
-                .chatRoomId(null) // DM/채팅 기능 붙을 때 매핑
-                .dmRequestPending(false) // 기본값, 후속 로직에서 세팅
-                .mainBadge(BadgeDto.from(user.getMainBadge()))
-                .abandonBadge(AbandonBadgeDto.from(user.getAbandonBadge()))
-                .build();
-    }
-
-    private static String resolvePrimaryPosition(User user) {
-        return user.getPositions().stream()
-                .filter(pos -> pos.getType() == UserPositionType.PRIMARY)
-                .map(UserPosition::getPositionName)
-                .findFirst()
-                .orElse(null);
-    }
 }
