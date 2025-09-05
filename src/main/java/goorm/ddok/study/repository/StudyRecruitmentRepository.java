a238be6ba376b2f780bd76eba07df64e0917dfda
projectmiluju <dnjsdyd0712@naver.com>
2025-09-05 12:45:05 +0900

    feat: 스터디 모집 검색 기능을 위한 페이징 지원 추가 DP-331

--- src/main/java/goorm/ddok/study/repository/StudyRecruitmentRepository.java
+++ src/main/java/goorm/ddok/study/repository/StudyRecruitmentRepository.java
@@ -2,6 +2,8 @@ package goorm.ddok.study.repository;
 
 import goorm.ddok.study.domain.TeamStatus;
 import goorm.ddok.study.domain.StudyRecruitment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -134,4 +136,7 @@ public interface StudyRecruitmentRepository extends JpaRepository<StudyRecruitme
           AND s.id = :id
         """, nativeQuery = true)
     Optional<StudyOverlayRow> findOverlayById(@Param("id") Long id);
+
+    Page<StudyRecruitment> findByDeletedAtIsNull(Pageable pageable);
+
 }
