acc77ece76d2629d51a1dbb68cdc9f1725e02885
sunsetkk <khyejin9796@gmail.com>
2025-09-05 15:21:23 +0900

    feature(team): 팀 지원자 응답 DTO 추가 DP-328

--- src/main/java/goorm/ddok/team/dto/response/TeamApplicantResponse.java
+++ src/main/java/goorm/ddok/team/dto/response/TeamApplicantResponse.java
@@ -1,2 +1,52 @@
-package goorm.ddok.team.dto.response;public class TeamApplicantResponse {
+package goorm.ddok.team.dto.response;
+
+import goorm.ddok.project.domain.ProjectApplication;
+import goorm.ddok.study.domain.StudyApplication;
+import goorm.ddok.team.domain.ApplicantStatus;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
+
+import java.time.Instant;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeamApplicantResponse {
+
+    private Long applicantId;
+    /* Study일 경우 null */
+    private String appliedPosition;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicantStatus status;
+
+    private Instant appliedAt;
+    private boolean IsMine;
+    private TeamApplicantUserResponse user;
+
+    // StudyApplication -> DTO
+    public static TeamApplicantResponse fromStudy(StudyApplication entity, Long currentUserId) {
+        return TeamApplicantResponse.builder()
+                .applicantId(entity.getId())
+                .appliedPosition(null) // Study는 appliedPosition 없음
+                .status(ApplicantStatus.from(entity.getApplicationStatus()))
+                .appliedAt(entity.getCreatedAt())
+                .IsMine(entity.getUser().getId().equals(currentUserId))
+                .user(TeamApplicantUserResponse.from(entity.getUser()))
+                .build();
+    }
+
+    // ProjectApplication -> DTO
+    public static TeamApplicantResponse fromProject(ProjectApplication entity, Long currentUserId) {
+        return TeamApplicantResponse.builder()
+                .applicantId(entity.getId())
+                .appliedPosition(entity.getPosition().getPositionName())
+                .status(ApplicantStatus.from(entity.getStatus()))
+                .appliedAt(entity.getCreatedAt())
+                .IsMine(entity.getUser().getId().equals(currentUserId))
+                .user(TeamApplicantUserResponse.from(entity.getUser()))
+                .build();
+    }
 }
