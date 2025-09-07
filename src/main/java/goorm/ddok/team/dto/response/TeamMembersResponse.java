8a1d553cee5866f4b2aef65a30a40d5db5864355
sunsetkk <khyejin9796@gmail.com>
2025-09-07 20:08:48 +0900

    feature(team): 참여 확정자 조회 API 기본 구조 추가 (Controller, Service, DTO) DP-345

---
+++ src/main/java/goorm/ddok/team/dto/response/TeamMembersResponse.java
@@ -0,0 +1,40 @@
+package goorm.ddok.team.dto.response;
+
+import goorm.ddok.chat.dto.response.PaginationResponse;
+import goorm.ddok.team.domain.TeamType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(
+        name = "TeamMembersResponse",
+        description = "팀 참여 확정자 목록 조회 응답 DTO"
+)
+public class TeamMembersResponse {
+
+    @Schema(description = "페이지네이션 정보", implementation = PaginationResponse.class)
+    private PaginationResponse pagination;
+
+    @Schema(description = "팀 ID", example = "2")
+    private Long teamId;
+
+    @Schema(description = "팀 타입 (PROJECT / STUDY)", example = "PROJECT")
+    private TeamType teamType;
+
+    @Schema(description = "팀 이름 (기본값: 모집글 제목)", example = "똑DDOK!")
+    private String teamTitle;
+
+    @Schema(description = "해당 팀이 연결된 모집글 ID", example = "33")
+    private Long recruitmentId;
+
+    @Schema(description = "현재 로그인한 사용자가 리더인지 여부", example = "true")
+    private boolean IsLeader;
+
+    @Schema(description = "팀 참여 확정자 목록", implementation = TeamMemberResponse.class)
+    private List<TeamMemberResponse> items;
+}
