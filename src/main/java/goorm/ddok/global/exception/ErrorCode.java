fac433f63e7921eb1ac77744addf18740fca59fd
projectmiluju <dnjsdyd0712@naver.com>
2025-09-10 08:33:21 +0900

    feat(error): 알림 수락/거절 허용 에러코드 생성 DP-358

--- src/main/java/goorm/ddok/global/exception/ErrorCode.java
+++ src/main/java/goorm/ddok/global/exception/ErrorCode.java
@@ -68,6 +68,7 @@ public enum ErrorCode {
     ALREADY_EXPELLED(HttpStatus.BAD_REQUEST, "이미 추방되었거나 탈퇴한 팀원입니다."),
     LEADER_CANNOT_BE_EXPELLED(HttpStatus.BAD_REQUEST, "리더는 추방할 수 없습니다."),
     LEADER_CANNOT_WITHDRAW(HttpStatus.BAD_REQUEST, "리더는 하차할 수 없습니다."),
+    NOT_ACTIONABLE(HttpStatus.BAD_REQUEST, "현재 상태에서 허용되지 않는 작업입니다."),
 
 
     // 401 UNAUTHORIZED
