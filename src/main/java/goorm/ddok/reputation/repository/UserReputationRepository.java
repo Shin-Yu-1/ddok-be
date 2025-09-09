e5804b720ee8db0f8e70995a57639e539feffef7
vayaconchoi <chlqjarms77@gmail.com>
2025-09-10 01:12:45 +0900

    feat(프로필 관련 온도 response 기능 추가): 프로필 관련 온도 response 기능 추가 DP-353

--- src/main/java/goorm/ddok/reputation/repository/UserReputationRepository.java
+++ src/main/java/goorm/ddok/reputation/repository/UserReputationRepository.java
@@ -3,7 +3,11 @@ package goorm.ddok.reputation.repository;
 import goorm.ddok.member.domain.User;
 import goorm.ddok.reputation.domain.UserReputation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserReputationRepository extends JpaRepository<UserReputation, Long> {
@@ -14,4 +18,7 @@ public interface UserReputationRepository extends JpaRepository<UserReputation, 
     Optional<UserReputation> findByUser_Id(Long userId);
 
     Optional<UserReputation> findByUser(User user);
+
+    @Query("select ur.user.id, ur.temperature from UserReputation ur where ur.user.id in :userIds")
+    List<Object[]> findTempsByUserIds(@Param("userIds") Collection<Long> userIds);
 }
