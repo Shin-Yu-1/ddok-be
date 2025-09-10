c65a97db99074f4161dd808a67f2052b168c0f02
projectmiluju <dnjsdyd0712@naver.com>
2025-09-10 15:41:39 +0900

    feat(notification): 알림 페이로드 DTO 추가 DP-358

---
+++ src/main/java/goorm/ddok/notification/dto/NotificationPayload.java
@@ -0,0 +1,30 @@
+package goorm.ddok.notification.dto;
+
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class NotificationPayload {
+    private String id;
+    private String type;
+    private String message;
+    private boolean isRead;
+    private Instant createdAt;
+
+    private String userId;
+    private String userNickname;
+
+    private String projectId;
+    private String projectTitle;
+    private String studyId;
+    private String studyTitle;
+
+    private String achievementName;
+    private String teamId;
+    private String teamName;
+
+    private BigDecimal userTemperature;
+}
