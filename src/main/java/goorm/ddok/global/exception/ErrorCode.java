95bc5aa63b7cbf8121abccf02b92c8df77567cbf
projectmiluju <dnjsdyd0712@naver.com>
2025-09-12 08:53:34 +0900

    feat(error): RECRUITMENT_CLOSED 에러 추가 DP-383

--- src/main/java/goorm/ddok/global/exception/ErrorCode.java
+++ src/main/java/goorm/ddok/global/exception/ErrorCode.java
@@ -69,6 +69,7 @@ public enum ErrorCode {
     LEADER_CANNOT_BE_EXPELLED(HttpStatus.BAD_REQUEST, "리더는 추방할 수 없습니다."),
     LEADER_CANNOT_WITHDRAW(HttpStatus.BAD_REQUEST, "리더는 하차할 수 없습니다."),
     CHAT_MESSAGE_INVALID(HttpStatus.BAD_REQUEST, "메세지 내용이 없습니다."),
+    RECRUITMENT_CLOSED(HttpStatus.BAD_REQUEST, "현재 모집 중이 아닙니다."),
 
 
     // 401 UNAUTHORIZED
