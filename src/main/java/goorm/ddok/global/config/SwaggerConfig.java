bf70b81a4d94c8a6b6c55d844451bffc66c17e81
projectmiluju <dnjsdyd0712@naver.com>
2025-09-17 09:29:30 +0900

    refactor(SwaggerConfig): 스웨거 설정 변경

--- src/main/java/goorm/ddok/global/config/SwaggerConfig.java
+++ src/main/java/goorm/ddok/global/config/SwaggerConfig.java
@@ -1,43 +1,30 @@
 package goorm.ddok.global.config;
 
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "DeepWebIDE API", version = "v1.1", description = "DeepWebIDE API 명세서"),
+        servers = {
+                @io.swagger.v3.oas.annotations.servers.Server(url = "http://localhost:8080", description = "로컬 서버"),
+                @Server(url = "https://api.deepdirect.site", description = "배포 서버")
+        },
+        security = @SecurityRequirement(name = "Authorization")
+)
+@SecurityScheme(
+        name = "Authorization",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
-    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
-
-    private OpenAPI base() {
-        return new OpenAPI()
-                .info(new Info().title("DDOK API").description("DDOK 프로젝트 REST API 문서").version("v1"))
-                .components(new Components()
-                        .addSecuritySchemes("Authorization",
-                                new SecurityScheme().name("Authorization")
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT"))
-                        .addSecuritySchemes("Reauth",
-                                new SecurityScheme().name("X-Reauth-Token")
-                                        .in(SecurityScheme.In.HEADER)
-                                        .type(SecurityScheme.Type.APIKEY)))
-                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
-    }
-
-    @Bean @org.springframework.context.annotation.Profile("local")
-    public OpenAPI openAPILocal() {
-        return base().servers(List.of(new Server().url("http://localhost:8080").description("Local")));
-    }
-
-    @Bean @org.springframework.context.annotation.Profile("prod")
-    public OpenAPI openAPIProd() {
-        return base().servers(List.of(new Server().url("https://api.deepdirect.site").description("Prod")));
-    }
 }
